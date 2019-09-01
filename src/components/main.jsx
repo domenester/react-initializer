19a3e42d25005ec4ef43cfb9b13d80b8b8866e2a
Diogo Domene <ddomene@interaxa.com>
2019-09-01 19:18:28 -0300

    Adding dotenv files

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Provider } from "react-redux" // eslint-disable-line no-unused-vars
+import { createStore } from "redux"
 import Home from "./routes/home/content" // eslint-disable-line no-unused-vars
 import { AppConfig } from "../config"
 import { BrowserRouter, Route } from 'react-router-dom'
@@ -10,7 +11,7 @@ document.title = AppConfig.title;
 export default class Main extends React.Component{
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(() => {})}>
         <BrowserRouter>
           <Route exact path="/" component={Home} />
         </BrowserRouter>
--- webpack.config.js
+++ webpack.config.js
@@ -3,71 +3,76 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
+const dotenv = require('dotenv');
 
-module.exports = (env) => ({
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  node: {process: true},
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
+module.exports = (env) => {
+  const dotenvsDefault = dotenv.config({ path: `./env/.env` }).parsed;
+  const dotenvs = dotenv.config({ path: `./env/.env.${env.NODE_ENV}` }).parsed;
+  return {
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    node: {process: true},
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.(png|jpg|woff|woff2|eot|ttf|svg|otf)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true }
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"]
+        },
+        {
+          test: /\.(png|jpg|woff|woff2|eot|ttf|svg|otf)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
         }
+      ]
+    },
+    devServer: {
+      port: 8080,
+      historyApiFallback: {
+        index: '/dist/index.html'
       }
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/views/index.html",
+        filename: "./index.html",
+        favicon: "./src/assets/images/favicon.ico"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new webpack.DefinePlugin({
+        'process': {
+          'env': JSON.stringify({...env, ...dotenvsDefault, ...dotenvs})
+        }
+      })
     ]
-  },
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: '/dist/index.html'
-    }
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/views/index.html",
-      filename: "./index.html",
-      favicon: "./src/assets/images/favicon.ico"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new webpack.DefinePlugin({
-      'process': {
-        'env': JSON.stringify(env)
-      }
-    })
-  ]
-});
+  }
+};
